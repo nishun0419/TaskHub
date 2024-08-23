7b1fc67ce726d6efd0d320bdd5a61662d7eb775c
SHUN <ninosekunlight@gmail.com>
2024-08-24 03:42:06 +0900

    goose用のマイグレーションファイルを作成

---
+++ backend/db/migrations/20240823165008_create_customers_table.sql
@@ -0,0 +1,15 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE `customers` (
+    `customer_id` INT AUTO_INCREMENT PRIMARY KEY,
+    `name` VARCHAR(255) NOT NULL,
+    `password` VARCHAR(255) NOT NULL,
+    `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    `updated_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE IF EXISTS `customers`;
+-- +goose StatementEnd
