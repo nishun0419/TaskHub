3ac7082409080eca683dbc39dcb01f6fa4600d62
SHUN <ninosekunlight@gmail.com>
2025-05-03 15:34:59 +0900

    定数を見る変更を入れるの忘れてた

--- frontend/constants/api.ts
+++ frontend/constants/api.ts
@@ -1,11 +1,11 @@
 export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL;
 
 export const API_ENDPOINTS = {
-  REGISTER: 'http://localhost:8080/api/register',
-  LOGIN: 'http://localhost:8080/api/login',
-  TEAMS: 'http://localhost:8080/api/team',
-  TEAM: (id: number) => `http://localhost:8080/api/team/${id}`,
-  TEAM_TODO: (teamID: number) => `http://localhost:8080/api/todo/team/${teamID}`,
-  TEAM_INVITE: (id: number) => `http://localhost:8080/api/team/${id}/invite`,
+  REGISTER: `${API_BASE_URL}/register`,
+  LOGIN: `${API_BASE_URL}/login`,
+  TEAMS: `${API_BASE_URL}/team`,
+  TEAM: (id: number) => `${API_BASE_URL}/team/${id}`,
+  TEAM_TODO: (teamID: number) => `${API_BASE_URL}/todo/team/${teamID}`,
+  TEAM_INVITE: (id: number) => `${API_BASE_URL}/team/${id}/invite`,
   // 他のエンドポイントもここに追加できます
 } as const; 
