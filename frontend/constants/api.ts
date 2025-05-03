0a496be543f1e5616469a23d35a3ed4d9ae0007b
SHUN <ninosekunlight@gmail.com>
2025-05-03 15:39:54 +0900

    設定ミス

--- frontend/constants/api.ts
+++ frontend/constants/api.ts
@@ -1,11 +1,11 @@
 export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL;
 
 export const API_ENDPOINTS = {
-  REGISTER: `${API_BASE_URL}/register`,
-  LOGIN: `${API_BASE_URL}/login`,
-  TEAMS: `${API_BASE_URL}/team`,
-  TEAM: (id: number) => `${API_BASE_URL}/team/${id}`,
-  TEAM_TODO: (teamID: number) => `${API_BASE_URL}/todo/team/${teamID}`,
-  TEAM_INVITE: (id: number) => `${API_BASE_URL}/team/${id}/invite`,
+  REGISTER: `${API_BASE_URL}/api/register`,
+  LOGIN: `${API_BASE_URL}/api/login`,
+  TEAMS: `${API_BASE_URL}/api/team`,
+  TEAM: (id: number) => `${API_BASE_URL}/api/team/${id}`,
+  TEAM_TODO: (teamID: number) => `${API_BASE_URL}/api/todo/team/${teamID}`,
+  TEAM_INVITE: (id: number) => `${API_BASE_URL}/api/team/${id}/invite`,
   // 他のエンドポイントもここに追加できます
 } as const; 
